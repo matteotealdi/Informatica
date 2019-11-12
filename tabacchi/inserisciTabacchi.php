3dc4ec162b4f0ddc0a60ab0123ed6a5c8e6c16ad
matteotealdi <57095602+matteotealdi@users.noreply.github.com>
2019-11-12 19:05:07 +0100

    Add files via upload

---
+++ tabacchi/inserisciTabacchi.php
@@ -0,0 +1,13 @@
+<?php
+
+    $marca = $_REQUEST["marca"];
+    $prezzo = $_REQUEST["prezzo"];
+
+
+    $debug = 0;
+    if($debug == 1){
+        echo "marca: " . $marca . " prezzo: " . $prezzo;
+    }
+
+
+?>
