e443e8f366a71e94f3df25d2d1effb511e638c6c
Matteo <57095602+matteotealdi@users.noreply.github.com>
2019-11-27 19:58:20 +0100

    Add files via upload

---
+++ Memory/start.php
@@ -0,0 +1,30 @@
+<html>
+
+    <head>
+
+
+    </head>
+    <body>
+
+            <table>
+                    <tr>
+                        <td> <img alt="" src="img/" height="100" id="imgClickAndChange" onclick="this.src='img/cane1.jpg'"  />     </td>
+                        <td> <img alt="" src="img/" height="100" id="imgClickAndChange" onclick="this.src='img/cane2.jpg'"  />    </td>
+                    </tr>
+                    <tr>
+                        <td> <img alt="" src="img/" height="100" id="imgClickAndChange" onclick="this.src='img/cane3.jpg'"  />   </td>
+                        <td><img alt="" src="img/" height="100" id="imgClickAndChange" onclick="this.src='img/cane1.jpg'"  />  </td>
+                    </tr>
+                    <tr>
+                        <td> <img alt="" src="img/" height="100" id="imgClickAndChange" onclick="this.src='img/cane2.jpg'"  />    </td>
+                        <td> <img alt="" src="img/" height="100" id="imgClickAndChange" onclick="this.src='img/cane3.jpg'"  />   </td>
+                    </tr>
+                    <tr>
+                        <td> <img alt="" src="img/" height="100" id="imgClickAndChange" onclick="this.src='img/cane1.jpg'"  />   </td>
+                        <td><img alt="" src="img/" height="100" id="imgClickAndChange" onclick="this.src='img/cane3.jpg'"  />  </td>
+                    </tr>
+                </table>
+        
+    </body>
+
+</html>
